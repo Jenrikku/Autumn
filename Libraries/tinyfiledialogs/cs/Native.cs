ad78c81ecfcc20df3cf98e25b6cee007ef9167da
Jenrikku <jenrikku@users.noreply.github.com>
2023-09-13 10:53:40 +0200

    Finished tinyfiledialogs bindings.

--- Libraries/tinyfiledialogs/cs/Native.cs
+++ Libraries/tinyfiledialogs/cs/Native.cs
@@ -6,9 +6,10 @@ internal static unsafe partial class Native
 {
     const string lib = "tinyfiledialogs";
 
-    // aCharVariableName: "tinyfd_version" "tinyfd_needs" "tinyfd_response"
-    // aIntVariableName : "tinyfd_verbose" "tinyfd_silent" "tinyfd_allowCursesDialogs"
-    //                    "tinyfd_forceConsole" "tinyfd_assumeGraphicDisplay" "tinyfd_winUtf8"
+    /* aCharVariableName: "tinyfd_version" "tinyfd_needs" "tinyfd_response"
+       aIntVariableName : "tinyfd_verbose" "tinyfd_silent" "tinyfd_allowCursesDialogs"
+                        "tinyfd_forceConsole" "tinyfd_assumeGraphicDisplay" "tinyfd_winUtf8"
+    */
 
     [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf8)]
     public static partial string? tinyfd_getGlobalChar(string aCharVariableName); // returns NULL on error
@@ -64,6 +65,27 @@ internal static unsafe partial class Native
         int aAllowMultipleSelects // 0 or 1
     ); // returns NULL on cancel, in case of multiple files, the separator is |
 
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial string? tinyfd_selectFolderDialog(
+        string? aTitle, // NULL or ""
+        string? aDefaultPath // NULL or ""
+    ); // returns NULL on cancel
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf8)]
+    public static partial string? tinyfd_colorChooser(
+        string? aTitle, // NULL or ""
+        string? aDefaultHexRGB, // NULL or "" or "#FF0000"
+        byte[] aDefaultRGB, // unsigned char lDefaultRGB[3] = { 0 , 128 , 255 };
+        byte[] aoResultRGB // unsigned char lResultRGB[3];
+    );
+
+    /* aDefaultRGB is used only if aDefaultHexRGB is absent
+       aDefaultRGB and aoResultRGB can be the same array
+       returns NULL on cancel
+       returns the hexcolor as a string "#FF0000"
+       aoResultRGB also contains the result
+    */
+
     [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
     public static partial int tinyfd_notifyPopupW(
         string? aTitle, // NULL or L""
@@ -79,4 +101,51 @@ internal static unsafe partial class Native
         string? aIconType, // L"info" L"warning" L"error" L"question"
         int aDefaultButton // 0 for cancel/no, 1 for ok/yes, 2 for no in yesnocancel
     ); // returns 0 for cancel/no, 1 for ok/yes, 2 for no in yesnocancel
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial string? tinyfd_inputBoxW(
+        string? aTitle, // NULL or L""
+        string? aMessage, // NULL or L"" (\n nor \t not respected)
+        string? aDefaultInput // NULL passwordBox, L"" inputbox
+    );
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial string? tinyfd_saveFileDialogW(
+        string? aTitle, // NULL or L""
+        string? aDefaultPathAndFile, // NULL or L""
+        int aNumOfFilterPatterns, // 0 (1 in the following example)
+        string[]? aFilterPatterns, // NULL or string? lFilterPatterns[1]={L"*.txt"}
+        string? aSingleFilterDescription // NULL or L"text files"
+    ); // returns NULL on cancel
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial string? tinyfd_openFileDialogW(
+        string? aTitle, // NULL or L""
+        string? aDefaultPathAndFile, // NULL or L""
+        int aNumOfFilterPatterns, // 0 (2 in the following example)
+        string[]? aFilterPatterns, // NULL or string? lFilterPatterns[2]={L"*.png","*.jpg"}
+        string? aSingleFilterDescription, // NULL or L"image files"
+        int aAllowMultipleSelects // 0 or 1
+    ); // returns NULL on cancel, in case of multiple files, the separator is |
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial string? tinyfd_selectFolderDialogW(
+        string? aTitle, // NULL or L""
+        string? aDefaultPath // NULL or L""
+    ); // returns NULL on cancel
+
+    [LibraryImport(lib, StringMarshalling = StringMarshalling.Utf16)]
+    public static partial string? tinyfd_colorChooserW(
+        string? aTitle, // NULL or L""
+        string? aDefaultHexRGB, // NULL or L"#FF0000"
+        byte[]? aDefaultRGB, // unsigned char lDefaultRGB[3] = { 0 , 128 , 255 };
+        byte[]? aoResultRGB // unsigned char lResultRGB[3];
+    );
+
+    /* returns the hexcolor as a string L"#FF0000"
+       aoResultRGB also contains the result
+       aDefaultRGB is used only if aDefaultHexRGB is NULL
+       aDefaultRGB and aoResultRGB can be the same array
+       returns NULL on cancel
+    */
 }
