a52a7d0e986742d803e9261c1bbcd2ac62dabf00
Jenrikku <jenrikku@users.noreply.github.com>
2023-09-22 11:10:59 +0200

    Remove debug project.

--- Autumn/Program.cs
+++ Autumn/Program.cs
@@ -5,14 +5,6 @@ using System.Text;
 
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-#if DEBUG
-RomFSHandler.RomFSPath = ""; // Please, input your RomFS manually here.
-
-StageHandler.TryImportStage("FirstStage", 1, out Stage stage);
-ProjectHandler.ActiveProject = new();
-ProjectHandler.ActiveProject.Stages.Add(stage);
-#endif
-
 SettingsHandler.LoadSettings();
 
 RomFSHandler.LoadFromSettings();
