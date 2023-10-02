780d94df7457919131282fde719ec7886d91cfcb
Jenrikku <jenrikku@users.noreply.github.com>
2023-10-02 11:34:06 +0200

    Implement RecentHandler

---
+++ Autumn/IO/RecentHandler.cs
@@ -0,0 +1,64 @@
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Autumn.GUI;
+
+namespace Autumn.IO;
+
+internal static class RecentHandler
+{
+    public static ObservableCollection<string> RecentOpenedPaths { get; } = new();
+
+    private static string _lastProjectOpenPath = string.Empty;
+    public static string LastProjectOpenPath
+    {
+        get => _lastProjectOpenPath;
+        set
+        {
+            _lastProjectOpenPath = value;
+            SettingsHandler.SetValue("LastProjectOpenPath", value);
+        }
+    }
+
+    private static string _lastProjectSavePath = string.Empty;
+    public static string LastProjectSavePath
+    {
+        get => _lastProjectSavePath;
+        set
+        {
+            _lastProjectSavePath = value;
+            SettingsHandler.SetValue("LastProjectSavePath", value);
+        }
+    }
+
+    private static string _lastProjectCreatePath = string.Empty;
+    public static string LastProjectCreatePath
+    {
+        get => _lastProjectCreatePath;
+        set
+        {
+            _lastProjectCreatePath = value;
+            SettingsHandler.SetValue("LastProjectCreatePath", value);
+        }
+    }
+
+    public static void LoadFromSettings()
+    {
+        string[] array = SettingsHandler.GetValue("RecentlyOpenedProjects", Array.Empty<string>())!;
+
+        RecentOpenedPaths.CollectionChanged -= OnRecentPathsChanged;
+
+        RecentOpenedPaths.Clear();
+
+        foreach (string str in array)
+            RecentOpenedPaths.Add(str);
+
+        RecentOpenedPaths.CollectionChanged += OnRecentPathsChanged;
+
+        _lastProjectOpenPath = SettingsHandler.GetValue("LastProjectOpenPath", string.Empty)!;
+        _lastProjectSavePath = SettingsHandler.GetValue("LastProjectSavePath", string.Empty)!;
+        _lastProjectCreatePath = SettingsHandler.GetValue("LastProjectCreatePath", string.Empty)!;
+    }
+
+    private static void OnRecentPathsChanged(object? sender, NotifyCollectionChangedEventArgs e) =>
+        SettingsHandler.SetValue("RecentlyOpenedProjects", RecentOpenedPaths.ToArray());
+}
