421946d23cc6b7897737513b4204c8f5f2d6a22b
Jenrikku <60291632+Jenrikku@users.noreply.github.com>
2025-02-25 17:22:21 +0100

    Rail point rendering

--- Autumn/Rendering/Rail/RailRenderer.cs
+++ Autumn/Rendering/Rail/RailRenderer.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Autumn.Enums;
 using Autumn.Rendering.DefaultCube;
 using Autumn.Rendering.Storage;
@@ -9,6 +10,8 @@ namespace Autumn.Rendering.Rail;
 
 internal static class RailRenderer
 {
+    private static readonly Matrix4x4 s_pointHandleScale = Matrix4x4.CreateScale(0.5f);
+
     private static RenderableModel? s_pointModel;
 
     public static void Initialize(GL gl) => s_pointModel = DefaultCubeRenderer.GenerateCubeModel(gl, 0.5f);
@@ -38,18 +41,32 @@ internal static class RailRenderer
         {
             case RailPointType.Bezier:
 
-                foreach (var point in rail.Points.Cast<RailPointBezier>())
+                for (int i = 0; i < rail.Points.Count; i++)
                 {
-                    // TO-DO: Draw bezier point.
+                    RailPointBezier point = (RailPointBezier)rail.Points[i];
+
+                    var pickingId = railSceneObj.PointsPickingIds[i];
+                    var selected = railSceneObj.PointsSelected[i];
+                    var transforms = railSceneObj.PointTransforms[i];
+
+                    DrawRailPoint(gl, scene, railPointMaterial, pickingId.P0, selected.P0, true, transforms.P0);
+                    DrawRailPoint(gl, scene, railPointMaterial, pickingId.P1, selected.P1, false, transforms.P1);
+                    DrawRailPoint(gl, scene, railPointMaterial, pickingId.P2, selected.P2, true, transforms.P2);
                 }
 
                 break;
 
             case RailPointType.Linear:
 
-                foreach (var point in rail.Points.Cast<RailPointLinear>())
+                for (int i = 0; i < rail.Points.Count; i++)
                 {
-                    // TO-DO: Draw linear point.
+                    RailPointLinear point = (RailPointLinear)rail.Points[i];
+
+                    var pickingId = railSceneObj.PointsPickingIds[i];
+                    var selected = railSceneObj.PointsSelected[i];
+                    var transforms = railSceneObj.PointTransforms[i];
+
+                    DrawRailPoint(gl, scene, railPointMaterial, pickingId.P1, selected.P1, false, transforms.P1);
                 }
 
                 break;
@@ -57,4 +74,31 @@ internal static class RailRenderer
     }
 
     public static void CleanUp(GL gl) => s_pointModel?.CleanUp(gl);
+
+    private static void DrawRailPoint(
+        GL gl,
+        CommonSceneParameters scene,
+        CommonMaterialParameters material,
+        uint pickingId,
+        bool selected,
+        bool isHandle,
+        Matrix4x4 transform
+    )
+    {
+        if (isHandle) transform = s_pointHandleScale * transform;
+
+        if (RailMaterial.Program.TryGetUniformLoc("uPickingId", out int location))
+            gl.Uniform1(location, pickingId);
+
+        scene.Transform = transform;
+        material.Selected = selected;
+
+        if (!RailMaterial.TryUse(gl, scene, material, out ProgramUniformScope scope))
+            return;
+
+        using (scope)
+        {
+            s_pointModel!.Draw(gl);
+        }
+    }
 }
